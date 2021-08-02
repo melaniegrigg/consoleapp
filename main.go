2aced45ea370ba2938ec20d4ea7d0716f37f8b7d
melaniegrigg <melanie.grigger@gmail.com>
2021-08-02 21:48:50 +0100

    menu exercise using rune and refectoring

--- main.go
+++ main.go
@@ -1,28 +1,55 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
+	"log"
+	"strconv"
 
 	"github.com/eiannone/keyboard"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	err := keyboard.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	//will execute as soon as the current function ends
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	coffees := make(map[int]string)
+	coffees[1] = "Cappucino"
+	coffees[2] = "Latte"
+	coffees[3] = "Americano"
+	coffees[4] = "Mocha"
+	coffees[5] = "Macchiato"
+	coffees[6] = "Espresso"
+
+	fmt.Println("Menu")
+	fmt.Println("_____")
+	fmt.Println("1 - Cappucino")
+	fmt.Println("2 - Latte")
+	fmt.Println("3 - Americano")
+	fmt.Println("4 - Mocha")
+	fmt.Println("5 - Machhiato")
+	fmt.Println("6 - Espresso")
+	fmt.Println("Q - Quit")
 
 	for {
-		fmt.Print("---> ")
-
-		userInput, _ := reader.ReadString('\n')
-
-		userInput = strings.Replace(userInput, "n", "", -1)
-
-		if userInput == "quit" {
-			break
-		} else {
-			fmt.Println(userInput)
+		char, _, err := keyboard.GetSingleKey()
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		if char == 'q' || char == 'Q' {
+			break
+		}
+
+		i, _ := strconv.Atoi(string(char))
+		fmt.Println(fmt.Sprintf("You chose %s", coffees[i]))
+
 	}
+
+	fmt.Println("program exit")
 }
